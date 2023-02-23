43706a1070eb103f2abed6421e7d54ff907063fd
Aishwarya K <102398704+aishk02@users.noreply.github.com>
2023-02-23 18:46:04 +0530

    Initial commit

---
+++ src/components/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+  {
+    title: "Home",
+    url: "/",
+    cName: "nav-links"
+  },
+  {
+    title: "Eat",
+    url: "/eat",
+    cName: "nav-links"
+  },
+  {
+    title: "Travel",
+    url: "/travel",
+    cName: "nav-links"
+  },
+  {
+    title: "Relax",
+    url: "/relax",
+    cName: "nav-links"
+  },
+  {
+    title: "Transport",
+    url: "/transport",
+    cName: "nav-links"
+  },
+  {
+    title: "Sign Up",
+    url: "/signup",
+    cName: "nav-links-mobile"
+  }
+];
